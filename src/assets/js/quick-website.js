f1ad1651ae3c9761af8a74b796f377f3652905f5
shonibare <47093379+shonibare@users.noreply.github.com>
2021-08-14 07:16:48 -0400

    third commit

--- src/assets/js/quick-website.js
+++ src/assets/js/quick-website.js
@@ -1,3 +1,18 @@
+/*
+
+Theme: Quick – Website UI Kit (FREE)
+Version: 1.1.0
+Product Page: https://webpixels.io/themes/quick-website-ui-kit
+License: MIT
+Author: Webpixels
+Author URI: https://webpixels.io
+
+---
+
+Copyright 2020 Webpixels
+
+*/
+
 //
 // Popover
 //
